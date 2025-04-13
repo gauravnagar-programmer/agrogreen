093fe5e4f5401f91a50e3458a83854eb03fad9ed
Gaurav Nagar <gauravnagar365@gmail.com>
2025-04-13 07:51:37 +0530

    commit all code of home page

---
+++ app.js
@@ -0,0 +1,12 @@
+const express  = require('express');
+const app= express();
+const path = require('path')
+
+app.set('view engine','ejs');
+app.use(express.static(path.join(__dirname,"public")));
+
+app.get('/',(req,res)=>{
+  res.render('home')
+});
+
+app.listen(4000) 
