7865b94fe81c65db3c15e77a7fa2956183c78bbf
Gaurav Nagar <gauravnagar365@gmail.com>
2025-04-13 16:43:32 +0530

    add about and contact page

--- app.js
+++ app.js
@@ -8,5 +8,11 @@ app.use(express.static(path.join(__dirname,"public")));
 app.get('/',(req,res)=>{
   res.render('home')
 });
+app.get('/about',(req,res)=>{
+  res.render('about')
+});
+app.get('/contact',(req,res)=>{
+  res.render('contact')
+});
 
 app.listen(4000) 
